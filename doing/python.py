9a16f3f5332b930d6a46a4270dffbd7c6cce887c
ggardusi <gustavo.gardusi@gmail.com>
2019-01-17 13:28:03 -0200

    commit

--- doing/python.py
+++ doing/python.py
@@ -1,6 +1,5 @@
 import atexit
 import io
-import math
 import sys
 
 _INPUT_LINES = sys.stdin.read().splitlines()
@@ -13,11 +12,6 @@ def write():
     sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
 
 def main():
-	h, w, x, y = list(map(int, input().split(' ')))
-	gcd = math.gcd(x, y)
-	x //= gcd
-	y //= gcd
-	ans = min(h // x, w // y)
-	print(str(x * ans) + ' ' + str(y * ans))
+	return 0
 
 main()
