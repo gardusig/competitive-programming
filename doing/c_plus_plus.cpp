886306097e9eb3bc0264eb14524a759262956f9d
ggardusi <gustavo.gardusi@gmail.com>
2019-01-30 17:41:25 -0200

    commit

--- doing/c_plus_plus.cpp
+++ doing/c_plus_plus.cpp
@@ -1,74 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(const auto &s, const auto &v) {
-  int lo = 1, hi = int(v.size());
-  while (lo <= hi) {
-    const auto mid = (lo + hi) / 2;
-    vector< pair< int, int > > ls;
-    for (int i = 0; i <= int(v.size()) - mid; i += 1) {
-      int l = (i == 0) ? 0 : v[i - 1] + 1;
-      int r = (i + mid < int(v.size())) ? v[i + mid] - 1 : int(s.size()) - 1;
-      ls.push_back({0, v[i + mid - 1] - v[i] + 1});
-      ls.push_back({1, r - l + 1});
-    }
-    sort(ls.begin(), ls.end());
-    int current = 0;
-    bool flag = false;
-    for (const auto &i: ls) {
-      if (i.first == 0) {
-        current += 1;
-      } else {
-        current -= 1;
-      }
-      flag |= (current > 1);
-    }
-    if (flag == true) {
-      lo = mid + 1;
-    } else {
-      hi = mid - 1;
-    }
-  }
-  return lo - 1;
-}
-
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  string s;
-  cin >> s;
-  int a = -1;
-  int b = -1;
-  int c = -1;
-  int d = -1;
-  if (s == string(int(s.size()), '0')) {
-    a = 1;
-    b = int(s.size()) - 1;
-    c = 2;
-    d = int(s.size());
+  freopen("electricity.in", "r", stdin);
+  freopen("electricity.out", "w", stdout);
+  int n, m;
+  cin >> n >> m;
+  vector< int > a(n), b(m);
+  for (int i = 0; i < n; i += 1) {
+    cin >> a[i];
   }
-  else {
-    vector< int > v;
-    for (int i = 0; i < int(s.size()); i += 1) {
-      if (s[i] == '1') {
-        v.push_back(i);
-      }
-    }
-    int kappaloiro = INT_MAX;
-    const auto mid = solve(s, v);
-    vector< pair< int, int > > ans;
-    for (int i = 0; i <= int(v.size()) - mid; i += 1) {
-      int l = (i == 0) ? 0 : v[i - 1] + 1;
-      int r = (i + mid < int(v.size())) ? v[i + mid] - 1 : int(s.size()) - 1;
-      ans.push_back({l, r});
-      kappaloiro = min(kappaloiro, r - l + 1);
-    }
-    a = ans[0].first + 1;
-    b = a + kappaloiro - 1;
-    c = ans[1].first + 1;
-    d = c + kappaloiro - 1;
+  for (int i = 0; i < m; i += 1) {
+    cin >> b[i];
   }
-  cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+  sort(a.rbegin(), a.rend());
+  sort(b.rbegin(), b.rend());
+  int ans = 1;
+  int a_have = 1;
+  int b_have = 0;
+  int i = 0, j = 0;
+  while (true) {
+    if ((a_have > 0 and i < m) == false) {
+      break;
+    }
+    while (a_have > 0 and i < m) {
+      a_have -= 1;
+      b_have += a[i];
+      i += 1;
+    }
+    if ((b_have > 0 and j < m) == false) {
+      break;
+    }
+    while (b_have > 0 and j < m) {
+      b_have -= 1;
+      a_have += b[j];
+      j += 1;
+    }
+    ans = max(ans, a_have);
+  }
+  cout << ans << '\n';
   return 0;
 }
--- doing/python.py
+++ doing/python.py
@@ -1,26 +1,23 @@
-import atexit
-import io
 import sys
 
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
+sys.stdin = open('aztec.in', 'r')
+sys.stdout = open('aztec.out', 'w')
 
-@atexit.register
-def write():
-  sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-def node:
-  def __init__(self):
-    self.nxt = []
-  node *nxt
+def solve(n):
+  sum = 1
+  i = 1
+  while True:
+    i += 1
+    current = 0
+    current += ((i + 1) * i) // 2
+    current += (i * (i - 1)) // 2
+    if sum + sum + current > n:
+      return i - 1
+    sum += current
 
 def main():
-  l, d, n = list(map(int, input().split(' ')))
-  for i in range(d):
-    s = input()
-
+  n = int(input())
+  print(solve(n))
   return 0
 
 main()
