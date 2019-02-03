8f85917288eb94dd54bf2534c18cfe00a852e0ed
ggardusi <gustavo.gardusi@gmail.com>
2019-02-03 10:07:59 -0200

    commit

--- doing/c_plus_plus.cpp
+++ doing/c_plus_plus.cpp
@@ -1,36 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1000000000 + 7;
-
-int exponentiate(const auto &n, const auto &k) {
-  if (k == 0) {
-    return 1;
-  }
-  int ans = exponentiate(n, k / 2);
-  ans = (1LL * ans * ans) % MOD;
-  if ((k & 1) != 0) {
-    ans = (1LL * ans * n) % MOD;
-  }
-  return ans;
-}
-
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int t;
-  cin >> t;
-  while (t-- > 0) {
-    int n, k, m;
-    cin >> n >> k >> m;
-    int a = exponentiate(n + k, MOD - 2);
-    int b = exponentiate(n, MOD - 2);
-    int c = (1LL * (n - 1) * b) % MOD;
-    int kappa = (1LL * ((m & 1) ? b : a) * exponentiate(c, m / 2)) % MOD;
-    int keepo = (1LL * (exponentiate(c, (m / 2)) - 1) * exponentiate(c - 1, MOD -2)) % MOD;
-    int ans = (1LL * b * keepo + kappa) % MOD;
-    cout << ans << '\n';
-  }
   return 0;
 }
