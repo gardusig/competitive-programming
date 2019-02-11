99ac38a450c962a345159ad616aed58e7dda0d6a
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2019-02-10 23:06:29 -0200

    Update c_plus_plus.cpp

--- templates/c_plus_plus.cpp
+++ templates/c_plus_plus.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+std::mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
