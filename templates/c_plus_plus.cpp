d7f72dac9215cf3951088e50417cc5f059577563
ggardusi <gustavo.gardusi@gmail.com>
2019-02-13 11:19:18 -0200

    .

--- templates/c_plus_plus.cpp
+++ templates/c_plus_plus.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-std::mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
-// std::shuffle(p.begin(), p.end(), rng);
+std::mt19937 rng(int(std::chrono::steady_clock::now().time_since_epoch().count()));
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.tie(0);
   return 0;
 }
