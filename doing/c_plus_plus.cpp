e4093b33d00bb19c2b779e02cb1a0503b5269a2d
ggardusi <gustavo.gardusi@gmail.com>
2019-03-04 01:50:57 -0300

    .

--- doing/c_plus_plus.cpp
+++ doing/c_plus_plus.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(const pair< int, double > &a, const pair< int, double > &b) {
-  double kappa = a.first + b.first * a.second;
-  double keepo = b.first + a.first * b.second;
-  return kappa < keepo;
-}
-
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int n, w;
-  cin >> n >> w;
-  vector< pair< int, double > > v;
-  for (int i = 0; i < n; i += 1) {
-    int c; double r;
-    cin >> c >> r;
-    v.push_back({c, r});
+  int t;
+  cin >> t;
+  while (t-- > 0) {
+    int n, k;
+    cin >> n >> k;
+    assert(n < 1123);
+    vector< int > v(n);
+    for (int i = 0; i < n; i += 1) {
+      cin >> v[i];
+    }
+    int ans = n;
+    for (int i = 0; i < n; i += 1) {
+      long long int current = 0;
+      for (int j = i; j < n; j += 1) {
+        current += v[j] / (j - i + 1);
+      }
+      if (current <= k) {
+        cout << "valid: " << i << ", current: " << current << endl;
+        ans = min(ans, i);
+      }
+    }
+    cout << ans + 1 << endl;
   }
-  sort(v.begin(), v.end(), compare);
-  double ans = 0;
-  double current = w;
-  for (const auto &i: v) {
-    ans += current * i.first;
-    current *= i.second;
-  }
-  cout << fixed << setprecision(10) << ans << endl;
   return 0;
 }
