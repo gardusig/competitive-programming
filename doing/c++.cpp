25db179e75b7e376ae6df18b31d37c903744b97f
ggardusi <gustavo.gardusi@gmail.com>
2019-03-05 16:45:15 -0300

    .

--- doing/c++.cpp
+++ doing/c++.cpp
@@ -5,5 +5,10 @@ int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i += 1) {
+    cout << n / i << endl;
+  }
   return 0;
 }
