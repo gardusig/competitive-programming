999c613243ce6f292bd9861b8b4c1043841a8c8d
ggardusi <gustavo.gardusi@gmail.com>
2019-03-09 17:49:16 -0300

    .

--- doing/c++.cpp
+++ doing/c++.cpp
@@ -1,30 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> cellCompete(int* states, int days) {
-  int size = 2 + (sizeof(states) / sizeof(states[0]));
-  vector< int > ans(size, 0), before(size, 0);
-  for (int i = 1; i < size - 1; i += 1) {
-    before[i] = states[i - 1];
-  }
-  for (int i = 0; i < days; i += 1) {
-    for (int j = 1; j < size - 1; j += 1) {
-      ans[j] = !(before[j - 1] == before[j + 1]);
-    }
-    before = ans;
-  }
-  ans.erase(ans.begin());
-  ans.erase(ans.end() - 1);
-  return ans;
-}
+mt19937 rng(int(chrono::steady_clock::now().time_since_epoch().count()));
 
 int main() {
-  int states[] = {1, 0, 0, 0, 0, 1, 0, 0};
-  for (int days = 1; days <= 100; days += 1) {
-    for (const auto &i: cellCompete(states, days)) {
-      cout << i << ' ';
-    }
-    cout << endl;
-  }
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
   return 0;
 }
