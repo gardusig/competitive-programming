6d921d8d3b44d3cfec8173d6da155ae716ef39f5
ggardusi <gustavo.gardusi@gmail.com>
2019-03-21 10:36:10 -0300

    training

--- doing/c++.cpp
+++ doing/c++.cpp
@@ -5,5 +5,27 @@ int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+  int n;
+  cin >> n;
+  vector< int > v(n);
+  unordered_map< int, vector< pair< int, int > > > g;
+  for (int i = 0; i < n; i += 1) {
+    cin >> v[i];
+  }
+  for (int i = 0; i < n; i += 1) {
+    int current = 0;
+    for (int j = i; j < n; j += 1) {
+      current += v[j];
+      g[current].push_back({i, j});
+    }
+  }
+  for (const auto &i: g) {
+    sort(g[i.first].begin(), g[i.first].end());
+    cout << i.first << ':';
+    for (const auto &j: i.second) {
+      cout << " {" << j.first << ", " << j.second << "}";
+    }
+    cout << endl;
+  }
   return 0;
 }
