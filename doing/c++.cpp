a953938e507f870d8276f507a6ea8107e7db0d83
ggardusi <gustavo.gardusi@gmail.com>
2019-03-26 21:04:58 -0300

    accepted :D

--- doing/c++.cpp
+++ doing/c++.cpp
@@ -5,27 +5,27 @@ int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int n;
-  cin >> n;
-  vector< int > v(n);
-  unordered_map< int, vector< pair< int, int > > > g;
-  for (int i = 0; i < n; i += 1) {
-    cin >> v[i];
-  }
-  for (int i = 0; i < n; i += 1) {
-    int current = 0;
-    for (int j = i; j < n; j += 1) {
-      current += v[j];
-      g[current].push_back({i, j});
+  int t;
+  cin >> t;
+  for (int test = 1; test <= t; test += 1) {
+    int n, p;
+    cin >> n >> p;
+    vector< int > v(n);
+    for (int i = 0; i < n; i += 1) {
+      cin >> v[i];
     }
-  }
-  for (const auto &i: g) {
-    sort(g[i.first].begin(), g[i.first].end());
-    cout << i.first << ':';
-    for (const auto &j: i.second) {
-      cout << " {" << j.first << ", " << j.second << "}";
+    sort(v.begin(), v.end());
+    long long int current = 0;
+    for (int i = 0; i < p - 1; i += 1) {
+      current += (v[p - 1] - v[i]);
     }
-    cout << endl;
+    long long int ans = current;
+    for (int i = p; i < n; i += 1) {
+      current -= (v[i - p + 1] - v[i - p]);
+      current += 1LL * (v[i] - v[i - 1]) * (p - 1);
+      ans = min(ans, current);
+    }
+    cout << "Case #" << test << ": " << ans << endl;
   }
   return 0;
 }
