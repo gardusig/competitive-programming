c1776233b69342d7ba66b0b17d92ce72d0aaaada
ggardusi <gustavo.gardusi@gmail.com>
2019-04-13 18:36:04 -0300

    .

--- doing/c++.cpp
+++ doing/c++.cpp
@@ -1,31 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+mt19937 rng(int(chrono::steady_clock::now().time_since_epoch().count()));
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int t;
-  cin >> t;
-  for (int test = 1; test <= t; test += 1) {
-    int n, p;
-    cin >> n >> p;
-    vector< int > v(n);
-    for (int i = 0; i < n; i += 1) {
-      cin >> v[i];
-    }
-    sort(v.begin(), v.end());
-    long long int current = 0;
-    for (int i = 0; i < p - 1; i += 1) {
-      current += (v[p - 1] - v[i]);
-    }
-    long long int ans = current;
-    for (int i = p; i < n; i += 1) {
-      current -= (v[i - p + 1] - v[i - p]);
-      current += 1LL * (v[i] - v[i - 1]) * (p - 1);
-      ans = min(ans, current);
-    }
-    cout << "Case #" << test << ": " << ans << endl;
-  }
   return 0;
 }
--- doing/python.py
+++ doing/python.py
@@ -12,18 +12,6 @@ def write():
   sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
 
 def main():
-  t = int(input())
-  for test in range(1, t + 1):
-    n, p = list(map(int, input().split(' ')))
-    v = list(map(int, input().split(' ')))
-    v.sort()
-    sum = [0 for i in range(n + 1)]
-    for i in range(1, n + 1):
-      sum[i] = sum[i - 1] + v[i - 1]
-    ans = 1123456789123456
-    for i in range(p, n + 1):
-      ans = min(ans, (v[i - 1] * p) - (sum[i] - sum[i - p]))
-    print('Case #' + str(test) + ': ' + str(ans))
   return 0
 
 main()
