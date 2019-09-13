a477c61c650d52ab5cd20d895a6b9f64c71cc911
Gustavo-Gardusi <gustavo.gardusi@gmail.com>
2019-09-13 13:23:00 -0300

    improve aho corasick from growing strings problem

--- reference/aho_corasick.cpp
+++ reference/aho_corasick.cpp
@@ -1,41 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e2 + 10;
-const int M = 150;
+const int N = 1e4 + 7;
+const int M = 26;
 
-int total;
-string s, x, y;
-int dp[N][N][N];
+int n;
+string s;
+int ans[N];
+stack<int> st;
+int idx, total;
+vector<int> g[N];
+vector<string> v;
 
 struct node {
 	int id;
-	bool end;
 	node *link;
 	node *nxt[M];
+	set<int> end;
 
 	node () {
-		end = false;
+		end.clear();
 		link = NULL;
 		id = total++;
 		for (int i = 0; i < M; ++i)
 			nxt[i] = NULL;
 	}
-	
+
 	void add (const int &i) {
 		if (i == s.length()) {
-			end = true;
+			end.insert(idx);
 			return;
 		}
-		if (!nxt[s[i]])
-			nxt[s[i]] = new node();
-		nxt[s[i]]->add(i + 1);
+		if (!nxt[s[i] - 'a'])
+			nxt[s[i] - 'a'] = new node();
+		nxt[s[i] - 'a']->add(i + 1);
 	}
-	
+
 	void build () {
 		queue<node*> q;
 		for (int i = 0; i < M; ++i) {
-			if (nxt[i])
-				nxt[i]->link = this, q.push(nxt[i]);
+			if (nxt[i]) {
+				nxt[i]->link = this;
+				nxt[i]->end.insert(end.begin(), end.end());
+				q.push(nxt[i]);
+			}
 			else
 				nxt[i] = this;
 		}
@@ -48,67 +55,82 @@ struct node {
 						fail = fail->link;
 					fail = fail->nxt[i];
 					current->nxt[i]->link = fail;
+					current->nxt[i]->end.insert(fail->end.begin(), fail->end.end());
 					q.push(current->nxt[i]);
 				}
 			}
 		}
 	}
-	
-	node* get (const char &c) {
+
+	void search (const int &i) {
+		for (set<int>::iterator j = end.begin(); j != end.end(); ++j)
+			if (idx != *j)
+				g[idx].push_back(*j);
+		if (i == s.size())
+			return;
+		int j = s[i] - 'a';
 		node *current = this;
-		while (!current->nxt[c] and current != current->link)
-			current = current->link;
-		if (current->nxt[c])
-			current = current->nxt[c];
-		return current;
+		while (!current->nxt[j] and current != current->link)
+			current = current->nxt[j];
+		current->nxt[j]->search(i + 1);
+	}
+
+	void clear () {
+		for (int i = 0; i < M; ++i)
+			if (nxt[i] and nxt[i]->id > id)
+				nxt[i]->clear();
+		delete this;
 	}
 };
 
-int solve (const int &i, const int &j, const int &k, node *current) {
-	if (i == x.size() or (j == y.size()))
-		return dp[i][j][k] = 0;
-	if (dp[i][j][k] != -1)
-		return dp[i][j][k];
-	int ans = 0;
-	ans = max(ans, solve(i + 1, j, k, current));
-	ans = max(ans, solve(i, j + 1, k, current));
-	current = current->get(x[i]);
-	if (x[i] == y[j] and !current->end)
-		ans = max(ans, 1 + solve(i + 1, j + 1, current->id, current));
-	return dp[i][j][k] = ans;
+void dfs (const int &i) {
+	ans[i] = 1;
+	for (int j = 0; j < g[i].size(); ++j)
+		if (!ans[g[i][j]])
+			dfs(g[i][j]);
+	st.push(i);
 }
 
-void path (const int &i, const int &j, const int &k, node *current) {
-	if (i == x.size() or (j == y.size()))
-		return;
-	if (dp[i][j][k] == dp[i + 1][j][k]) {
-		path(i + 1, j, k, current);
-		return;
+int solve () {
+	for (int i = 0; i < n; ++i)
+		if (!ans[i])
+			dfs(i);
+	for (; st.size(); st.pop()) {
+		int i = st.top();
+		for (int j = 0; j < g[i].size(); ++j)
+			ans[g[i][j]] = max(ans[g[i][j]], ans[i] + 1);
 	}
-	if (dp[i][j][k] == dp[i][j + 1][k]) {
-		path(i, j + 1, k, current);
-		return;
-	}
-	current = current->get(x[i]);
-	s.push_back(x[i]);
-	path(i + 1, j + 1, current->id, current);
+	int best = 0;
+	for (int i = 0; i < n; ++i)
+		best = max(best, ans[i]);
+	return best;
 }
 
 int main () {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	cin >> x >> y >> s;
-	total = 0;
-	node *root = new node();
-	root->add(0);
-	root->build();
-	memset(dp, -1, sizeof dp);
-	int ans = solve(0, 0, 0, root);
-	if (!ans) {
-		cout << 0 << '\n';
-		return 0;
+	while (cin >> n) {
+		if (!n)
+			break;
+		total = 0;
+		v.clear();
+		node *root = new node();
+		for (int i = 0; i < n; ++i)
+			ans[i] = 0, g[i].clear();
+		getline(cin, s);
+		for (int i = 0; i < n; ++i) {
+			idx = i;
+			getline(cin, s);
+			root->add(0);
+			v.push_back(s);
+		}
+		root->build();
+		for (int i = 0; i < v.size(); ++i) {
+			idx = i;
+			s = v[i];
+			root->search(0);
+		}
+		cout << solve() << '\n';
+		root->clear();
 	}
-	s.clear();
-	path(0, 0, 0, root);
-	cout << s << '\n';
 	return 0;
 }
