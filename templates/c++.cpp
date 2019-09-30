26209de025f293bc24727b2990a710e46ef47138
Gustavo-Gardusi <gustavo.gardusi@gmail.com>
2019-09-30 12:23:07 -0300

    improve template

--- templates/c++.cpp
+++ templates/c++.cpp
@@ -1,32 +1,28 @@
-// <template>
-
 #include <bits/stdc++.h>
-
-std::mt19937 rng(int(std::chrono::steady_clock::now().time_since_epoch().count()));
-
-// </template>
 
 class Solution
 {
-private:
-  // shuffle(v.begin(), v.end(), rng);
-
 public:
-  Solution()
-  {
-    std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0);
-    std::cout.tie(0);
-  }
 
-  void main()
-  {
+    Solution()
+    {
+        std::ios_base::sync_with_stdio(0);
+        std::cin.tie(0);
+        std::cout.tie(0);
+    }
 
-  }
+    void main()
+    {
+
+    }
+
+private:
+
+
 };
 
 int main()
 {
-  Solution().main();
-  return 0;
+    Solution().main();
+    return 0;
 }
