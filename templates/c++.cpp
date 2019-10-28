b39266e981c3ca5ba8196754d58fe7e0eca45c18
developer4fun <gustavo.gardusi@gmail.com>
2019-10-28 13:55:03 -0300

    update tab size

--- templates/c++.cpp
+++ templates/c++.cpp
@@ -3,25 +3,22 @@
 class Solution
 {
 public:
+  Solution()
+  {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+  }
 
-    Solution()
-    {
-        std::ios_base::sync_with_stdio(0);
-        std::cin.tie(0);
-        std::cout.tie(0);
-    }
-
-    void main()
-    {
-
-    }
+  void main()
+  {
+  }
 
 private:
-
 };
 
 int main()
 {
-    Solution().main();
-    return 0;
+  Solution().main();
+  return 0;
 }
