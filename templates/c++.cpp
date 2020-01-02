a4487bcff574101e51c55b0ea82cf3add272caac
developer4fun <gustavo.gardusi@gmail.com>
2020-01-01 22:58:55 -0300

    update template

--- templates/c++.cpp
+++ templates/c++.cpp
@@ -1,24 +1,9 @@
 #include <bits/stdc++.h>
 
-class Solution
-{
-public:
-  Solution()
-  {
-    std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0);
-    std::cout.tie(0);
-  }
-
-  void main()
-  {
-  }
-
-private:
-};
-
 int main()
 {
-  Solution().main();
-  return 0;
+	std::ios_base::sync_with_stdio();
+	std::cin.tie(0);
+	std::cout.tie(0);
+	return 0;
 }
--- templates/python.py
+++ templates/python.py
@@ -1,5 +1,3 @@
-# <template>
-
 import atexit
 import io
 import sys
@@ -9,20 +7,22 @@ input = iter(_INPUT_LINES).__next__
 _OUTPUT_BUFFER = io.StringIO()
 sys.stdout = _OUTPUT_BUFFER
 
+
 @atexit.register
 def write():
-  sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
 
-# </template>
 
 class Solution:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def main(self):
-    pass
+    def main(self):
+        pass
+
 
 def main():
-  Solution().main()
+    Solution().main()
+
 
 main()
