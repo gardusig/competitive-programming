37ab82708b05d45d0aa57375869912b07cccff8e
binaryLifter <gustavo.gardusi@gmail.com>
2020-02-19 14:59:16 -0300

    add explanation to code

--- reference/palindrome-counting.cpp
+++ reference/palindrome-counting.cpp
@@ -2,6 +2,7 @@
  
 std::vector<int> v;
  
+// return amount of distinct palindrome numbers <= x
 long long int query(const long long int x)
 {
 	if (x == 0)
@@ -9,6 +10,7 @@ long long int query(const long long int x)
 		return 0;
 	}
  
+	// build vector of digits
 	v.clear();
 	for (long long int i = x; i > 0; i /= 10)
 	{
@@ -19,25 +21,28 @@ long long int query(const long long int x)
 	long long int ans = 0;
 	const int size = (int(v.size()) + 1) >> 1;
  
+	// add amount of palindromes with size 1 == 2, 3 == 4, 5 == 6 ..
 	long long int pot = 1;
 	for (int i = 1; i < size; i += 1, pot *= 10)
 	{
 		ans += ((9LL * pot) << 1);
 	}
  
+	// if size is even, add previous odd
 	if (!(int(v.size()) & 1))
 	{
 		ans += (9LL * pot);
 	}
  
+	// add number of palindromes less than x with the same size
 	ans += 1LL * (v[0] - 1) * pot;
 	pot /= 10;
- 
 	for (int i = 1; i < size; i += 1, pot /= 10)
 	{
 		ans += 1LL * v[i] * pot;
 	}
  
+	// if it is possible to form a palindrome with prefix of x, add 1
 	bool valid = true;
 	for (int i = (int(v.size()) >> 1) - 1; i >= 0; i -= 1)
 	{
