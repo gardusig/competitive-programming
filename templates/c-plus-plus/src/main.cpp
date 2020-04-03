76f463a31cc1f21345d7331983cca7ffbaa7ce37
binaryLifter <gustavo.gardusi@gmail.com>
2020-04-03 19:52:10 -0300

    remove unused files

--- templates/c-plus-plus/src/main.cpp
+++ templates/c-plus-plus/src/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <memory>
 
 int main()
 {
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
     std::cout.tie(0);
-
-    std::unique_ptr<int> ptr(new int(1));
-
     return 0;
 }
