937da207d8cf63086373444e107c9e7b880ab508
ggardusi <gustavo.gardusi@gmail.com>
2020-10-04 18:25:32 -0300

    refactor

--- reference/cpp_tricks/fast scanf.cpp
+++ reference/cpp_tricks/fast scanf.cpp
@@ -1,31 +1,16 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-void scanint (int &x) {
-    x = 0;
-    register int c = getchar_unlocked();
-    for (; c < 48 or c > 57; c = getchar_unlocked());
-    for (; c > 47 and c < 58; c = getchar_unlocked())
-        x = (x << 1) + (x << 3) + c - 48;
+int read_integer() {
+  int N = 0;
+  register int c = '$';
+  while (!isdigit(c)) {
+    c = getchar_unlocked();
+  }
+  while (isdigit(c)) {
+    N = (N << 1) + (N << 3) + c - '0';
+    c = getchar_unlocked();
+  }
+  return N;
 }
 
-void scanint_negative (int &x) {
-    x = 0;
-    bool negative = false;
-    register int c = getchar_unlocked();
-    for (; (c < 48 or c > 57) and c != '-'; c = getchar_unlocked());
-    if (c == '-')
-        negative = true, c = getchar_unlocked();
-    for (; c > 47 and c < 58; c = getchar_unlocked())
-        x = (x << 1) + (x << 3) + c - 48;
-    if (negative)
-        x = -x;
-}
-
-int main () {
-    int x; scanint(x);
-    int y; scanint_negative(y);
-    printf("x = %d\n", x);
-    printf("y = %d\n", y);
-    return 0;
-}
+int main() { return 0; }
