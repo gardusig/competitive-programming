427fcb16b7082984b2283ade5d3083685913d38e
ggardusi <gustavo.gardusi@gmail.com>
2021-07-13 21:11:35 -0300

    remove method implementations and default value assignment from class header declaration

--- src/data_structures/fenwick-tree.cpp
+++ src/data_structures/fenwick-tree.cpp
@@ -5,41 +5,61 @@
 template <typename T>
 class FenwickTree {
  public:
-  explicit FenwickTree(const int size) : accum(size, this->DEFAULT_VALUE) {}
+  explicit FenwickTree(const int size);
 
-  void resize(const int size) {
-    if (size < 0) {
-      throw std::invalid_argument("size should not be negative");
-    }
-    this->accum.resize(size);
-  }
+  void clear();
+  void resize(const int size);
 
-  void reset() { std::fill(accum.begin(), accum.end(), this->DEFAULT_VALUE); }
-
-  void update(const int idx, const T value) {
-    if (idx <= 0) {
-      throw std::exception("idx should be positive");
-    }
-    if (idx >= static_cast<int>(accum.size())) {
-      throw std::exception("idx out of range");
-    }
-    for (int i = idx; i < static_cast<int>(accum.size()); i += i & (-i)) {
-      this->accum[i] += value;
-    }
-  }
-
-  T query(const int idx) const {
-    if (idx >= static_cast<int>(this->accum.size())) {
-      throw std::exception("idx out of range");
-    }
-    T ans = 0;
-    for (int i = idx; i > 0; i -= i & (-i)) {
-      ans += this->accum[i];
-    }
-    return ans;
-  }
+  T query(const int idx) const;
+  void update(const int idx, const T value);
 
  private:
   std::vector<T> accum;
-  const T DEFAULT_VALUE = 0;
+
+  static const T DEFAULT_VALUE;
 };
+
+template <typename T>
+const T FenwickTree<T>::DEFAULT_VALUE = 0;
+
+template <typename T>
+FenwickTree<T>::FenwickTree(const int size)
+    : accum(size, FenwickTree::DEFAULT_VALUE) {}
+
+template <typename T>
+void FenwickTree<T>::resize(const int size) {
+  if (size < 0) {
+    assert(false);
+  }
+  this->accum.resize(size);
+}
+
+template <typename T>
+void FenwickTree<T>::clear() {
+  std::fill(accum.begin(), accum.end(), FenwickTree::DEFAULT_VALUE);
+}
+
+template <typename T>
+void FenwickTree<T>::update(const int idx, const T value) {
+  if (idx <= 0) {
+    assert(false);
+  }
+  if (idx >= static_cast<int>(accum.size())) {
+    assert(false);
+  }
+  for (int i = idx; i < static_cast<int>(accum.size()); i += i & (-i)) {
+    this->accum[i] += value;
+  }
+}
+
+template <typename T>
+T FenwickTree<T>::query(const int idx) const {
+  if (idx >= static_cast<int>(this->accum.size())) {
+    assert(false);
+  }
+  T ans = 0;
+  for (int i = idx; i > 0; i -= i & (-i)) {
+    ans += this->accum[i];
+  }
+  return ans;
+}
