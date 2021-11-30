c5c0143a030c1a90fd7a0d658782f080a3b2d77c
ggardusi <gustavo.gardusi@gmail.com>
2021-11-30 20:00:04 -0300

    minor code style improvements to be compliant with cpplint

--- src/mathematics/math-library.cpp
+++ src/mathematics/math-library.cpp
@@ -15,16 +15,18 @@ class Math {
 void Math::build_lowest_factors(const int size) {
   assert(size > 0);
   this->lowest_factors.resize(size);
-  for (int i = 2; i < int(lowest_factors.size()); i += 1) {
+  for (int i = 2; i < static_cast<int>(lowest_factors.size()); i += 1) {
     lowest_factors[i] = (i & 1) ? i : 2;
   }
   primes.push_back(2);
-  for (int prime = 3; prime < int(lowest_factors.size()); prime += 2) {
+  for (int prime = 3; prime < static_cast<int>(lowest_factors.size());
+       prime += 2) {
     if (lowest_factors[prime] != prime) {
       continue;
     }
     primes.push_back(prime);
-    for (int multiple = prime; multiple < int(lowest_factors.size());
+    for (int multiple = prime;
+         multiple < static_cast<int>(lowest_factors.size());
          multiple += prime) {
       if (lowest_factors[multiple] != multiple) {
         continue;
@@ -36,7 +38,7 @@ void Math::build_lowest_factors(const int size) {
 
 std::unordered_map<int, int> Math::get_factors(const int N) const {
   assert(N > 0);
-  assert(N < int(lowest_factors.size()));
+  assert(N < static_cast<int>(lowest_factors.size()));
   std::unordered_map<int, int> factors;
   for (int i = N; i > 1; i /= lowest_factors[i]) {
     factors[lowest_factors[i]] += 1;
@@ -52,7 +54,7 @@ std::unordered_map<int64_t, int> Math::get_factors_slow(int64_t N) const {
       factors[prime] += 1;
       N /= prime;
     }
-    if (N == 1 or N < prime) {
+    if (N == 1 || N < prime) {
       break;
     }
   }
