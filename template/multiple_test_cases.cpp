f36c79a6884827ee9d26d0cf691a4c6c5f4f3f5a
gardusig <gustavo.gardusi@gmail.com>
2023-01-22 19:51:20 -0300

    Created multiple test cases example

---
+++ template/multiple_test_cases.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+void solve_test_case(const int test_case) {
+  std::cout << "implement your solution here :)" << std::endl;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  int test_cases;
+  std::cin >> test_cases;
+  for (int test_case = 1; test_case <= test_cases; test_case += 1) {
+    solve_test_case(test_case);
+  }
+  return 0;
+}
